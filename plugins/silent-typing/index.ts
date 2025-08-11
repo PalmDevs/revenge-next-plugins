d70283c5d197be13c399e553408678b1649a26a8
PalmDevs <git@palmdevs.me>
2025-08-11 20:20:42 +0700

    chore: format

--- plugins/silent-typing/index.ts
+++ plugins/silent-typing/index.ts
@@ -4,18 +4,18 @@ import { PluginFlags } from '@revenge-mod/plugins/constants'
 import { noop } from '@revenge-mod/utils/callback'
 
 registerPlugin(
-	{
-		id: 'palmdevs.silent-typing',
-		name: 'Silent Typing',
-		description: 'Disables the typing indicator when you type.',
-		author: 'Palm',
-		icon: 'KeyboardIcon',
-	},
-	{
-		start({ cleanup }) {
-			cleanup(onFluxEventDispatched('TYPING_START', noop))
-		},
-	},
-	PluginFlags.Enabled,
-	0,
+    {
+        id: 'palmdevs.silent-typing',
+        name: 'Silent Typing',
+        description: 'Disables the typing indicator when you type.',
+        author: 'Palm',
+        icon: 'KeyboardIcon',
+    },
+    {
+        start({ cleanup }) {
+            cleanup(onFluxEventDispatched('TYPING_START', noop))
+        },
+    },
+    PluginFlags.Enabled,
+    0,
 )
--- types/next-plugins-internal.d.ts
+++ types/next-plugins-internal.d.ts
@@ -1,14 +1,14 @@
 import type {
-	PluginApiExtensionsOptions,
-	PluginDependency,
-	PluginManifest,
-	PluginOptions,
+    PluginApiExtensionsOptions,
+    PluginDependency,
+    PluginManifest,
+    PluginOptions,
 } from './next/lib/plugins/types'
 
 // Internal import
 export function registerPlugin<O extends PluginApiExtensionsOptions>(
-	manifest: PluginManifest,
-	options: PluginOptions<O>,
-	flags?: number,
-	iflags?: number,
+    manifest: PluginManifest,
+    options: PluginOptions<O>,
+    flags?: number,
+    iflags?: number,
 ): PluginDependency
